2b594bd209803efd390f7cc165ecdef0a378e641
Spxg <unsafe@outlook.es>
2023-09-13 22:07:41 +0800

    Add problem 0002: Add Two Numbers

---
+++ src/problem_0002_add_two_numbers/mod.rs
@@ -0,0 +1,35 @@
+pub mod zip;
+
+use crate::data_structures::ListNode;
+
+pub trait Solution {
+    fn add_two_numbers(l1: Option<Box<ListNode>>, l2: Option<Box<ListNode>>) -> Option<Box<ListNode>>;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Solution;
+    use crate::test_utilities;
+
+    pub fn run<S: Solution>() {
+        let test_cases = [
+            ((&[2, 4, 3] as &[_], &[5, 6, 4] as &[_]), &[7, 0, 8] as &[_]),
+            ((&[0], &[0]), &[0]),
+            ((&[9, 9, 9, 9, 9, 9, 9], &[9, 9, 9, 9]), &[8, 9, 9, 9, 0, 0, 0, 1]),
+            ((&[9, 9, 9, 9], &[9, 9, 9, 9, 9, 9, 9]), &[8, 9, 9, 9, 0, 0, 0, 1]),
+        ];
+
+        for ((l1, l2), expected) in test_cases {
+            assert_eq!(
+                test_utilities::iter_list(&S::add_two_numbers(
+                    test_utilities::make_list(l1.iter().copied()),
+                    test_utilities::make_list(l2.iter().copied())
+                ))
+                .copied()
+                .collect::<Box<_>>()
+                .as_ref(),
+                expected
+            );
+        }
+    }
+}
---
+++ src/problem_0002_add_two_numbers/zip.rs
@@ -0,0 +1,55 @@
+pub struct Solution;
+
+use crate::data_structures::ListNode;
+
+impl Solution {
+    pub fn add_two_numbers(l1: Option<Box<ListNode>>, l2: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
+        let mut l1 = l1;
+        let mut l2 = l2;
+        let mut carry = 0;
+        let mut result = ListNode::new(0);
+        let mut ref_result = &mut result;
+
+        loop {
+            match (&mut l1, &mut l2) {
+                (Some(lhs), Some(rhs)) => {
+                    let sum = lhs.val + rhs.val + carry;
+                    carry = sum / 10;
+                    ref_result.next = Some(Box::new(ListNode::new(sum % 10)));
+                    ref_result = ref_result.next.as_mut().unwrap();
+                }
+                (Some(node), None) | (None, Some(node)) => {
+                    let sum = node.val + carry;
+                    carry = sum / 10;
+                    ref_result.next = Some(Box::new(ListNode::new(sum % 10)));
+                    ref_result = ref_result.next.as_mut().unwrap();
+                }
+                (None, None) => {
+                    if carry == 0 {
+                        break;
+                    }
+                    ref_result.next = Some(Box::new(ListNode::new(carry)));
+                    break;
+                }
+            }
+            l1 = l1.and_then(|mut x| x.next.take());
+            l2 = l2.and_then(|mut x| x.next.take());
+        }
+
+        result.next
+    }
+}
+
+impl super::Solution for Solution {
+    fn add_two_numbers(l1: Option<Box<ListNode>>, l2: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
+        Self::add_two_numbers(l1, l2)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_solution() {
+        super::super::tests::run::<super::Solution>();
+    }
+}
