817aa75788c3dd55b20edeeb3604ed75bbfb27bc
Spxg <unsafe@outlook.es>
2023-10-11 13:17:01 +0800

    Add problem 0080: Remove Duplicates from Sorted Array II

--- src/lib.rs
+++ src/lib.rs
@@ -158,6 +158,7 @@ pub mod problem_0066_plus_one;
 pub mod problem_0067_add_binary;
 pub mod problem_0070_climbing_stairs;
 pub mod problem_0071_simplify_path;
+pub mod problem_0080_remove_duplicates_from_sorted_array_ii;
 
 #[cfg(test)]
 pub mod test_utilities;
---
+++ src/problem_0080_remove_duplicates_from_sorted_array_ii/windows.rs
@@ -0,0 +1,34 @@
+pub struct Solution;
+
+impl Solution {
+    pub fn remove_duplicates(nums: &mut Vec<i32>) -> i32 {
+        let mut remove_idxs = vec![];
+        for (idx, element) in nums.windows(3).enumerate() {
+            if let [prev, mid, last] = *element {
+                if prev == mid && prev == last {
+                    remove_idxs.push(idx + 2);
+                }
+            }
+        }
+
+        remove_idxs.iter().enumerate().for_each(|(idx, remove_idx)| {
+            nums.remove(remove_idx - idx);
+        });
+
+        nums.len() as i32
+    }
+}
+
+impl super::Solution for Solution {
+    fn remove_duplicates(nums: &mut Vec<i32>) -> i32 {
+        Self::remove_duplicates(nums)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_solution() {
+        super::super::tests::run::<super::Solution>();
+    }
+}
