a1efc5001302720e7dc5dbb2817259622790d08c
Spxg <unsafe@outlook.es>
2023-11-03 16:30:58 +0800

    Add problem 0031: Next Permutation

---
+++ src/problem_0031_next_permutation/iterative.rs
@@ -0,0 +1,33 @@
+pub struct Solution;
+
+impl Solution {
+    pub fn next_permutation(nums: &mut Vec<i32>) {
+        for idx1 in (0..nums.len()).rev() {
+            let slice = &mut nums[idx1..];
+            for last in (0..slice.len()).rev() {
+                for idx2 in (0..last).rev() {
+                    if slice[idx2] < slice[last] {
+                        slice.swap(idx2, last);
+                        slice[idx2 + 1..].reverse();
+                        return;
+                    }
+                }
+            }
+        }
+        nums.reverse();
+    }
+}
+
+impl super::Solution for Solution {
+    fn next_permutation(nums: &mut Vec<i32>) {
+        Self::next_permutation(nums);
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_solution() {
+        super::super::tests::run::<super::Solution>();
+    }
+}
---
+++ src/problem_0031_next_permutation/mod.rs
@@ -0,0 +1,29 @@
+pub mod iterative;
+
+pub trait Solution {
+    fn next_permutation(nums: &mut Vec<i32>);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Solution;
+
+    pub fn run<S: Solution>() {
+        let test_cases = [
+            (&[1, 2, 3] as &[_], &[1, 3, 2] as &[_]),
+            (&[3, 2, 1], &[1, 2, 3]),
+            (&[1, 1, 5], &[1, 5, 1]),
+            (&[1], &[1]),
+            (&[1, 5, 1], &[5, 1, 1]),
+            (&[1, 3, 2], &[2, 1, 3]),
+        ];
+
+        for (nums, expected) in test_cases {
+            let mut nums = nums.to_vec();
+
+            S::next_permutation(&mut nums);
+
+            assert_eq!(nums.as_slice(), expected);
+        }
+    }
+}
