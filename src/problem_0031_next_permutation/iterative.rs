c3ccb355ef068bafd6097b592584cdea24ab35aa
Spxg <unsafe@outlook.es>
2023-11-05 00:09:05 +0800

    Update Next Permutation

--- src/problem_0031_next_permutation/iterative.rs
+++ src/problem_0031_next_permutation/iterative.rs
@@ -5,7 +5,7 @@ impl Solution {
         for idx1 in (0..nums.len()).rev() {
             let slice = &mut nums[idx1..];
             for last in (0..slice.len()).rev() {
-                for idx2 in (0..last).rev() {
+                for idx2 in 0..last {
                     if slice[idx2] < slice[last] {
                         slice.swap(idx2, last);
                         slice[idx2 + 1..].reverse();
