d768e4fa3037ba634bcd92bf549eae237c163c69
Spxg <unsafe@outlook.es>
2023-11-28 12:32:47 +0800

    Add problem 0104: Maximum Depth of Binary Tree

---
+++ src/problem_0104_maximum_depth_of_binary_tree/mod.rs
@@ -0,0 +1,34 @@
+pub mod recursive;
+
+use crate::data_structures::TreeNode;
+
+use std::cell::RefCell;
+use std::rc::Rc;
+
+pub trait Solution {
+    fn max_depth(root: Option<Rc<RefCell<TreeNode>>>) -> i32;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Solution;
+    use crate::test_utilities;
+
+    pub fn run<S: Solution>() {
+        let test_cases = [
+            (
+                &[Some(3), Some(9), Some(20), None, None, Some(15), Some(7)] as &[_],
+                3,
+            ),
+            (&[], 0),
+            (&[Some(3)], 1),
+        ];
+
+        for (root, expected) in test_cases {
+            assert_eq!(
+                S::max_depth(test_utilities::make_tree(root.iter().copied())),
+                expected
+            );
+        }
+    }
+}
---
+++ src/problem_0104_maximum_depth_of_binary_tree/recursive.rs
@@ -0,0 +1,31 @@
+pub struct Solution;
+
+use crate::data_structures::TreeNode;
+
+use std::cell::RefCell;
+use std::rc::Rc;
+
+impl Solution {
+    pub fn max_depth(root: Option<Rc<RefCell<TreeNode>>>) -> i32 {
+        fn helper(node: &Option<Rc<RefCell<TreeNode>>>, depth: i32) -> i32 {
+            node.as_ref().map_or(depth, |node| {
+                helper(&node.borrow().left, depth + 1).max(helper(&node.borrow().right, depth + 1))
+            })
+        }
+        helper(&root, 0)
+    }
+}
+
+impl super::Solution for Solution {
+    fn max_depth(root: Option<Rc<RefCell<TreeNode>>>) -> i32 {
+        Self::max_depth(root)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_solution() {
+        super::super::tests::run::<super::Solution>();
+    }
+}
