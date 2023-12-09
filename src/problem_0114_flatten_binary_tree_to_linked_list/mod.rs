4fdb363a1eaa272c8d814137fb0069886c46501f
Spxg <unsafe@outlook.es>
2023-12-09 18:23:12 +0800

    Add problem 0114: Flatten Binary Tree to Linked List

---
+++ src/problem_0114_flatten_binary_tree_to_linked_list/mod.rs
@@ -0,0 +1,47 @@
+pub mod morris;
+
+use crate::data_structures::TreeNode;
+
+use std::cell::RefCell;
+use std::rc::Rc;
+
+pub trait Solution {
+    fn flatten(root: &mut Option<Rc<RefCell<TreeNode>>>);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Solution;
+    use crate::test_utilities;
+
+    pub fn run<S: Solution>() {
+        let test_cases = [
+            (
+                &[Some(1), Some(2), Some(5), Some(3), Some(4), None, Some(6)] as &[_],
+                &[
+                    Some(1),
+                    None,
+                    Some(2),
+                    None,
+                    Some(3),
+                    None,
+                    Some(4),
+                    None,
+                    Some(5),
+                    None,
+                    Some(6),
+                ] as &[_],
+            ),
+            (&[], &[]),
+            (&[Some(0)], &[Some(0)]),
+        ];
+
+        for (root, expected) in test_cases {
+            let mut root = test_utilities::make_tree(root.iter().copied());
+
+            S::flatten(&mut root);
+
+            assert_eq!(root, test_utilities::make_tree(expected.iter().copied()));
+        }
+    }
+}
---
+++ src/problem_0114_flatten_binary_tree_to_linked_list/morris.rs
@@ -0,0 +1,43 @@
+pub struct Solution;
+
+use crate::data_structures::TreeNode;
+
+use std::cell::RefCell;
+use std::rc::Rc;
+
+impl Solution {
+    pub fn flatten(root: &mut Option<Rc<RefCell<TreeNode>>>) {
+        let mut root = root.clone();
+        while let Some(node) = root {
+            let mut node = node.borrow_mut();
+            if let Some(mut right_leaf) = node.left.clone() {
+                loop {
+                    let right = right_leaf.borrow().right.clone();
+                    if let Some(right) = right {
+                        right_leaf = right;
+                    } else {
+                        break;
+                    }
+                }
+                let mut right_leaf = right_leaf.borrow_mut();
+                right_leaf.right = node.right.take();
+                node.right = node.left.take();
+            }
+            root = node.right.clone();
+        }
+    }
+}
+
+impl super::Solution for Solution {
+    fn flatten(root: &mut Option<Rc<RefCell<TreeNode>>>) {
+        Self::flatten(root);
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_solution() {
+        super::super::tests::run::<super::Solution>();
+    }
+}
