7e74eea2d3c0c5ac76291f497e5f385207da48c6
Spxg <unsafe@outlook.es>
2023-12-31 11:54:23 +0800

    Add problem 0155: Min Stack

---
+++ src/problem_0155_min_stack/min_stack.rs
@@ -0,0 +1,56 @@
+pub struct MinStack {
+    inner: Vec<(i32, i32)>,
+}
+
+impl MinStack {
+    fn new() -> Self {
+        Self { inner: vec![] }
+    }
+
+    fn push(&mut self, val: i32) {
+        let min = self.inner.last().map_or(val, |&(_, min)| min.min(val));
+        self.inner.push((val, min));
+    }
+
+    fn pop(&mut self) {
+        self.inner.pop().unwrap();
+    }
+
+    fn top(&self) -> i32 {
+        self.inner.last().unwrap().0
+    }
+
+    fn get_min(&self) -> i32 {
+        self.inner.last().unwrap().1
+    }
+}
+
+impl super::MinStack for MinStack {
+    fn new() -> Self {
+        Self::new()
+    }
+
+    fn push(&mut self, x: i32) {
+        self.push(x);
+    }
+
+    fn pop(&mut self) {
+        self.pop();
+    }
+
+    fn top(&self) -> i32 {
+        self.top()
+    }
+
+    fn get_min(&self) -> i32 {
+        self.get_min()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_solution() {
+        super::super::tests::run::<super::MinStack>();
+    }
+}
