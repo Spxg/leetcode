d2bdbdf4ce658d40a1d60f5cb08519f92308c914
Spxg <unsafe@outlook.es>
2024-01-14 12:10:42 +0800

    Add problem 0274: H-Index

---
+++ src/problem_0274_h_index/mod.rs
@@ -0,0 +1,18 @@
+pub mod sort;
+
+pub trait Solution {
+    fn h_index(citations: Vec<i32>) -> i32;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Solution;
+
+    pub fn run<S: Solution>() {
+        let test_cases = [(&[3, 0, 6, 1, 5] as &[_], 3), (&[1, 3, 1], 1), (&[0], 0)];
+
+        for (citations, expected) in test_cases {
+            assert_eq!(S::h_index(citations.to_vec()), expected);
+        }
+    }
+}
---
+++ src/problem_0274_h_index/sort.rs
@@ -0,0 +1,32 @@
+pub struct Solution;
+
+impl Solution {
+    pub fn h_index(citations: Vec<i32>) -> i32 {
+        let mut result = 0;
+        let mut citations = citations;
+
+        citations.sort_unstable_by(|a, b| b.cmp(a));
+        for (idx, val) in (1..).zip(citations) {
+            if idx > val {
+                break;
+            }
+            result = idx;
+        }
+
+        result
+    }
+}
+
+impl super::Solution for Solution {
+    fn h_index(citations: Vec<i32>) -> i32 {
+        Self::h_index(citations)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_solution() {
+        super::super::tests::run::<super::Solution>();
+    }
+}
