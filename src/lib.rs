a24ee347b2e7704cb941f63c24fddc078d9e00eb
Spxg <unsafe@outlook.es>
2024-04-02 13:21:06 +0800

    Add problem 0561: Array Partition

--- src/lib.rs
+++ src/lib.rs
@@ -307,6 +307,7 @@ pub mod problem_0541_reverse_string_ii;
 pub mod problem_0551_student_attendance_record_i;
 pub mod problem_0557_reverse_words_in_a_string_iii;
 pub mod problem_0560_subarray_sum_equals_k;
+pub mod problem_0561_array_partition;
 pub mod problem_0567_permutation_in_string;
 pub mod problem_0623_add_one_row_to_tree;
 pub mod problem_0643_maximum_average_subarray_i;
---
+++ src/problem_0561_array_partition/mod.rs
@@ -0,0 +1,18 @@
+pub mod sort;
+
+pub trait Solution {
+    fn array_pair_sum(nums: Vec<i32>) -> i32;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Solution;
+
+    pub fn run<S: Solution>() {
+        let test_cases = [(&[1, 4, 3, 2] as &[_], 4), (&[6, 2, 6, 5, 1, 2], 9)];
+
+        for (nums, expected) in test_cases {
+            assert_eq!(S::array_pair_sum(nums.to_vec()), expected);
+        }
+    }
+}
