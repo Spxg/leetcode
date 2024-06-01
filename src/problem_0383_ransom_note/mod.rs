37b2b0d81fa19188d56a4c28730516bca08cd957
Spxg <unsafe@outlook.es>
2024-06-01 12:04:12 +0800

    Add problem 0383: Ransom Note

---
+++ src/problem_0383_ransom_note/mod.rs
@@ -0,0 +1,25 @@
+pub mod letters_count;
+
+pub trait Solution {
+    fn can_construct(ransom_note: String, magazine: String) -> bool;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Solution;
+
+    pub fn run<S: Solution>() {
+        let test_cases = [
+            (("a", "b"), false),
+            (("aa", "ab"), false),
+            (("aa", "aab"), true),
+        ];
+
+        for ((ransom_note, magazine), expected) in test_cases {
+            assert_eq!(
+                S::can_construct(ransom_note.to_string(), magazine.to_string()),
+                expected
+            );
+        }
+    }
+}
