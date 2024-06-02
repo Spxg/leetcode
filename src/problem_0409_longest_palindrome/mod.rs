0e7445daa06e0d5df56c2feedce7d8f1022eb256
Spxg <unsafe@outlook.es>
2024-06-02 12:29:47 +0800

    Add problem 0409: Longest Palindrome

---
+++ src/problem_0409_longest_palindrome/mod.rs
@@ -0,0 +1,18 @@
+pub mod iterative;
+
+pub trait Solution {
+    fn longest_palindrome(s: String) -> i32;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Solution;
+
+    pub fn run<S: Solution>() {
+        let test_cases = [("abccccdd", 7), ("a", 1), ("bb", 2)];
+
+        for (s, expected) in test_cases {
+            assert_eq!(S::longest_palindrome(s.to_string()), expected);
+        }
+    }
+}
