65b71ed3d4caa4801c6a18b21e86e225dcf04aac
Spxg <unsafe@outlook.es>
2024-06-03 14:13:38 +0800

    Add problem 0481: Magical String

---
+++ src/problem_0481_magical_string/mod.rs
@@ -0,0 +1,31 @@
+pub mod queue;
+
+pub trait Solution {
+    fn magical_string(n: i32) -> i32;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Solution;
+
+    pub fn run<S: Solution>() {
+        // https://oeis.org/A156077.
+        let test_cases = [
+            (1, 1),
+            (2, 1),
+            (3, 1),
+            (4, 2),
+            (5, 3),
+            (6, 3),
+            (7, 4),
+            (8, 4),
+            (9, 4),
+            (10, 5),
+            (100_000, 49972),
+        ];
+
+        for (n, expected) in test_cases {
+            assert_eq!(S::magical_string(n), expected);
+        }
+    }
+}
---
+++ src/problem_0481_magical_string/queue.rs
@@ -0,0 +1,38 @@
+pub struct Solution;
+
+use std::collections::VecDeque;
+
+impl Solution {
+    pub fn magical_string(n: i32) -> i32 {
+        let mut count = 3;
+        let mut result = 1;
+        let mut prev = 2;
+        let mut queue = VecDeque::from([2]);
+
+        while let Some(x) = queue.pop_front() {
+            if count >= n {
+                break;
+            }
+            count += x;
+            prev = 1 + i32::from(prev == 1);
+            queue.extend(std::iter::repeat(prev).take(x as usize));
+            result += if count > n { 1 } else { x } * i32::from(prev == 1);
+        }
+
+        result
+    }
+}
+
+impl super::Solution for Solution {
+    fn magical_string(n: i32) -> i32 {
+        Self::magical_string(n)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_solution() {
+        super::super::tests::run::<super::Solution>();
+    }
+}
