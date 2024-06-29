f30021f66c12b162aebe9f2a1739e63c4457b863
Spxg <unsafe@outlook.es>
2024-06-29 12:22:46 +0800

    Add problem 0637: Average of Levels in Binary Tree

---
+++ src/problem_0637_average_of_levels_in_binary_tree/mod.rs
@@ -0,0 +1,40 @@
+pub mod bfs;
+
+use crate::data_structures::TreeNode;
+
+use std::cell::RefCell;
+use std::rc::Rc;
+
+pub trait Solution {
+    fn average_of_levels(root: Option<Rc<RefCell<TreeNode>>>) -> Vec<f64>;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Solution;
+    use crate::test_utilities;
+
+    pub fn run<S: Solution>() {
+        let test_cases = [
+            (
+                &[Some(3), Some(9), Some(20), None, Some(15), Some(7)] as &[_],
+                &[3.0, 14.5, 11.0] as &[_],
+            ),
+            (
+                &[Some(3), Some(9), Some(20), Some(15), Some(7)],
+                &[3.0, 14.5, 11.0],
+            ),
+            (
+                &[Some(0x7FFF_FFFF), Some(0x7FFF_FFFF), Some(0x7FFF_FFFF)],
+                &[2_147_483_647.0, 2_147_483_647.0],
+            ),
+        ];
+
+        for (root, expected) in test_cases {
+            assert_eq!(
+                S::average_of_levels(test_utilities::make_tree(root.iter().copied())),
+                expected
+            );
+        }
+    }
+}
