20df876b118be4776906d058bb114394ac85a73d
Spxg <unsafe@outlook.es>
2024-08-05 14:04:08 +0800

    Add problem 0947: Most Stones Removed with Same Row or Column

---
+++ src/problem_0947_most_stones_removed_with_same_row_or_column/mod.rs
@@ -0,0 +1,25 @@
+pub mod hash_map;
+
+pub trait Solution {
+    fn remove_stones(stones: Vec<Vec<i32>>) -> i32;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Solution;
+
+    pub fn run<S: Solution>() {
+        let test_cases = [
+            (&[[0, 0], [0, 1], [1, 0], [1, 2], [2, 1], [2, 2]] as &[_], 5),
+            (&[[0, 0], [0, 2], [1, 1], [2, 0], [2, 2]], 3),
+            (&[[0, 0]], 0),
+        ];
+
+        for (stones, expected) in test_cases {
+            assert_eq!(
+                S::remove_stones(stones.iter().map(Vec::from).collect()),
+                expected
+            );
+        }
+    }
+}
