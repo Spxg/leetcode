b1853e1fe35d92fed1d9a7252e385baea09f33a4
Spxg <unsafe@outlook.es>
2024-09-15 20:08:23 +0800

    Add problem 0938: Range Sum of BST

---
+++ src/problem_0938_range_sum_of_bst/recursive.rs
@@ -0,0 +1,40 @@
+pub struct Solution;
+
+use crate::data_structures::TreeNode;
+
+use std::cell::RefCell;
+use std::rc::Rc;
+
+impl Solution {
+    pub fn range_sum_bst(root: Option<Rc<RefCell<TreeNode>>>, low: i32, high: i32) -> i32 {
+        fn helper(root: &Option<Rc<RefCell<TreeNode>>>, low: i32, high: i32) -> i32 {
+            root.as_ref().map_or(0, |node| {
+                let val = node.borrow().val;
+                if (low..=high).contains(&val) {
+                    val + helper(&node.borrow().right, low, high)
+                        + helper(&node.borrow().left, low, high)
+                } else if val < low {
+                    helper(&node.borrow().right, low, high)
+                } else {
+                    helper(&node.borrow().left, low, high)
+                }
+            })
+        }
+
+        helper(&root, low, high)
+    }
+}
+
+impl super::Solution for Solution {
+    fn range_sum_bst(root: Option<Rc<RefCell<TreeNode>>>, low: i32, high: i32) -> i32 {
+        Self::range_sum_bst(root, low, high)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_solution() {
+        super::super::tests::run::<super::Solution>();
+    }
+}
