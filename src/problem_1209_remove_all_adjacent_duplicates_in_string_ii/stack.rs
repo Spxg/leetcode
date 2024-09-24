ea1e3bbca12b4179a12264555d472fe489b8ac40
Spxg <unsafe@outlook.es>
2024-09-24 13:14:34 +0800

    Add problem 1209: Remove All Adjacent Duplicates in String II

---
+++ src/problem_1209_remove_all_adjacent_duplicates_in_string_ii/stack.rs
@@ -0,0 +1,37 @@
+pub struct Solution;
+
+impl Solution {
+    pub fn remove_duplicates(s: String, k: i32) -> String {
+        let mut stack = Vec::with_capacity(s.len());
+        for ch in s.chars() {
+            if let Some((prev, count)) = stack.last_mut() {
+                if *prev == ch {
+                    *count += 1;
+                    if *count == k {
+                        stack.pop();
+                    }
+                    continue;
+                }
+            }
+            stack.push((ch, 1));
+        }
+        stack
+            .into_iter()
+            .flat_map(|(ch, count)| std::iter::repeat(ch).take(count as usize))
+            .collect()
+    }
+}
+
+impl super::Solution for Solution {
+    fn remove_duplicates(s: String, k: i32) -> String {
+        Self::remove_duplicates(s, k)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_solution() {
+        super::super::tests::run::<super::Solution>();
+    }
+}
