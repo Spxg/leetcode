0a95b0417fbc31f61ade0e372892b31080e886c7
Spxg <unsafe@outlook.es>
2024-10-10 13:04:37 +0800

    Add problem 1310: XOR Queries of a Subarray

--- src/lib.rs
+++ src/lib.rs
@@ -520,6 +520,7 @@ pub mod problem_1268_search_suggestions_system;
 pub mod problem_1282_group_the_people_given_the_group_size_they_belong_to;
 pub mod problem_1291_sequential_digits;
 pub mod problem_1302_deepest_leaves_sum;
+pub mod problem_1310_xor_queries_of_a_subarray;
 pub mod problem_2348_number_of_zero_filled_subarrays;
 
 #[cfg(test)]
