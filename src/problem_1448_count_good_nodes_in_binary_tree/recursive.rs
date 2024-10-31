2701ec7c9c8e9d56f564ac5912ce432493d44253
Spxg <unsafe@outlook.es>
2024-10-31 12:48:53 +0800

    Add problem 1448: Count Good Nodes in Binary Tree

---
+++ src/problem_1448_count_good_nodes_in_binary_tree/recursive.rs
@@ -0,0 +1,37 @@
+pub struct Solution;
+
+use crate::data_structures::TreeNode;
+
+use std::cell::RefCell;
+use std::rc::Rc;
+
+impl Solution {
+    pub fn good_nodes(root: Option<Rc<RefCell<TreeNode>>>) -> i32 {
+        fn helper(node: &Option<Rc<RefCell<TreeNode>>>, max: i32, result: &mut i32) {
+            let Some(node) = node else { return };
+            let val = node.borrow().val;
+            if val >= max {
+                *result += 1;
+            }
+            helper(&node.borrow().left, max.max(val), result);
+            helper(&node.borrow().right, max.max(val), result);
+        }
+        let mut result = 0;
+        helper(&root, i32::MIN, &mut result);
+        result
+    }
+}
+
+impl super::Solution for Solution {
+    fn good_nodes(root: Option<Rc<RefCell<TreeNode>>>) -> i32 {
+        Self::good_nodes(root)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_solution() {
+        super::super::tests::run::<super::Solution>();
+    }
+}
