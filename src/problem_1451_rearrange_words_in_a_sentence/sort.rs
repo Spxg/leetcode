15037bc6f4812b873f8ec0e3b340a898e4ba1c8d
Spxg <unsafe@outlook.es>
2024-11-03 15:17:36 +0800

    Add problem 1451: Rearrange Words in a Sentence

---
+++ src/problem_1451_rearrange_words_in_a_sentence/sort.rs
@@ -0,0 +1,30 @@
+pub struct Solution;
+
+impl Solution {
+    pub fn arrange_words(text: String) -> String {
+        let mut text = text.into_bytes();
+        text[0].make_ascii_lowercase();
+
+        let mut words = text.split(|&x| x == b' ').collect::<Vec<_>>();
+        words.sort_unstable_by_key(|word| (word.len(), word.as_ptr()));
+
+        let mut result = words.join(&b' ');
+        result[0].make_ascii_uppercase();
+
+        String::from_utf8(result).unwrap()
+    }
+}
+
+impl super::Solution for Solution {
+    fn arrange_words(text: String) -> String {
+        Self::arrange_words(text)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_solution() {
+        super::super::tests::run::<super::Solution>();
+    }
+}
