754f135fe98acaf98b2bfed1fb282779e016a57d
Spxg <unsafe@outlook.es>
2025-02-02 21:13:32 +0800

    Fix clippy

--- src/problem_1078_occurrences_after_bigram/iterative.rs
+++ src/problem_1078_occurrences_after_bigram/iterative.rs
@@ -7,9 +7,8 @@ impl Solution {
         let mut split2 = text.split(' ').skip(1);
         loop {
             match (split1.next(), split2.next()) {
-                (None, None) => break,
+                (Some(_) | None, None) => break,
                 (None, Some(_)) => unreachable!(),
-                (Some(_), None) => break,
                 (Some(x), Some(y)) => {
                     if x == first && y == second {
                         if let Some(n) = split2.clone().next() {
